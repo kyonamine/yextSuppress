774158ac62ff2b2d7b919eb12749b53d54f98c0b
Kevin Yonamine <kyonamine@yext.com>
2024-09-18 12:21:21 -0400

    Update suppression.py

--- suppression.py
+++ suppression.py
@@ -18,13 +18,12 @@ credentials = {
 authorized_user = {"refresh_token": st.secrets["refresh_token"], "token_uri": "https://oauth2.googleapis.com/token", "client_id": st.secrets["client_id"], "client_secret": st.secrets["client_secret"], "scopes": ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"], "expiry": "2022-11-09T18:33:12.530288Z"}
 gc, authorized_user = gspread.oauth_from_dict(credentials, authorized_user)
 
-
-
 def getPubs():
     pubsUrl = 'https://docs.google.com/spreadsheets/d/1q1QcCD_wXY2QMMphScptTlSFOnsWA-35072U510oWVk/edit?gid=0#gid=0' #prod doc
     sh = gc.open_by_url(pubsUrl)
     worksheet = sh.get_worksheet(0)
     pubList = worksheet.col_values(1)
+    pubList = pubList.pop(0)
     os.write(1,  f"{pubList}\n".encode())
 
 getPubs()
