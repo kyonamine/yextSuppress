16e5323490899e8bcf6e42db9570b6aa8d51a271
Kevin Yonamine <kyonamine@yext.com>
2024-09-18 12:00:49 -0400

    Update suppression.py

--- suppression.py
+++ suppression.py
@@ -1,5 +1,17 @@
 import requests
 import os
+import streamlit as st
 
-print(os.environ['GOOGLESHEETCREDS'])
+credentials = {
+    "installed": {
+        "client_id": st.secrets["client_id"],
+        "project_id": st.secrets["project_id"],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", 
+        "client_secret": st.secrets["client_secret"],
+        "redirect_uris":["http://localhost"]
+    }    
+}
 
+print(credentials)
